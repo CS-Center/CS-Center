f276a0ca797cc7b034bba14c73741a6ee606e4db
Riolku <keenan@thegugelers.ca>
2019-10-09 21:23:41 -0400

    Move emails into another thread

--- wcics/mail/utils.py
+++ wcics/mail/utils.py
@@ -2,14 +2,25 @@
 
 # Mailing utilities
 
-from wcics import mail_app
+import threading
+
+from wcics import mail_app, app
 
 # Send a message to a single person
 def send_single(addr, subject = "", body = ""):
-  mail_app.send_message(subject, recipients = [addr], html = body)
+  def inner():    
+    # You need an app context for this :/
+    with app.app_context():
+      mail_app.send_message(subject, recipients = [addr], html = body)
+    
+  threading.Thread(target = inner).start()
     
 # Send a message to many people
 def send_many(addrs, subject = "", body = ""):
-  with mail_app.connect() as conn:
-    for addr in addrs:
-      conn.send_message(subject, recipients = [addr], html = body)
+  def inner():
+    with app.app_context():
+      with mail_app.connect() as conn:
+        for addr in addrs:
+          conn.send_message(subject, recipients = [addr], html = body)
+  
+  threading.Thread(target = inner).start()
