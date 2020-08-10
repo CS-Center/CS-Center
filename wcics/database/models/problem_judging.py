d1193dee2ff94c9d146762905fb64f6b03b27f40
Riolku <keenan@thegugelers.ca>
2020-08-10 13:39:08 -0400

    Update problem db

--- wcics/database/models/problem_judging.py
+++ wcics/database/models/problem_judging.py
@@ -9,32 +9,25 @@ class ProblemSuites(dbmodel, Helper):
   id = dbcol(dbint, primary_key = True)
   pid = dbcol(dbint, dbforkey(Problems.id, onupdate = "CASCADE", ondelete = "CASCADE"), nullable = False)
   suite = dbcol(dbint, nullable = False)
-  
+
   points = dbcol(dbint, nullable = False)
-  
+
   __tablename__ = "problem_suites"
 
 class ProblemCases(dbmodel, Helper):
   pid = dbcol(dbint, dbforkey(Problems.id, onupdate = "CASCADE", ondelete = "CASCADE"), nullable = False)
   sid = dbcol(dbint, dbforkey(ProblemSuites.id, onupdate = "CASCADE", ondelete = "CASCADE"), nullable = False)
-  
+
   input = dbcol(dbstr(PROBLEM_INPUT_MAX_LENGTH), nullable = False)
   output = dbcol(dbstr(PROBLEM_OUTPUT_MAX_LENGTH), nullable = False)
-  
+
   case = dbcol(dbint, nullable = False)
-  
+
   __tablename__ = "problem_cases"
-  
+
 class ProblemJudging(dbmodel, Helper):
   pid = dbcol(dbint, dbforkey(Problems.id, onupdate = "CASCADE", ondelete = "CASCADE"), nullable = False, unique = True)
 
   checker_id = dbcol(dbstr(ID_MAX_LENGTH), nullable = False)
-  checker_code = dbcol(dbstr(CODE_MAX_LENGTH))
-  checker_lang = dbcol(dbstr(LANG_MAX_LENGTH))
-  checker_arg = dbcol(dbstr(ARG_MAX_LENGTH), nullable = False)
-  
-  custom_interactor = dbcol(dbbool, nullable = False)
-  interactor_code = dbcol(dbstr(CODE_MAX_LENGTH))
-  interactor_lang = dbcol(dbstr(LANG_MAX_LENGTH))
-  
+
   __tablename__ = "problem_judging"
--- wcics/database/models/problems.py
+++ wcics/database/models/problems.py
@@ -12,20 +12,20 @@ class Problems(dbmodel, Helper):
   id = dbcol(dbint, primary_key = True)
   pid = dbcol(dbstr(PROBLEM_PID_MAX_LENGTH), nullable = False)
   oid = dbcol(dbint, dbforkey(Organizations.id, onupdate = "CASCADE", ondelete = "CASCADE"), nullable = False)
-  
+
   memory_limit = dbcol(dblong, nullable = False)
   timelimit = dbcol(dbfloat, nullable = False)
-  
+
   # Problem sections
   title = dbcol(psection, nullable = False)
   desc = dbcol(psection, nullable = False)
   constraints = dbcol(psection, nullable = False)
   input_specs = dbcol(psection, nullable = False)
   output_specs = dbcol(psection, nullable = False)
-  
+
   # force (pid, oid) to be unique
   __table_args__ = (dbunicon('pid', 'oid'), )
-  
+
   __tablename__ = "problems"
 
 class ProblemAuthors(dbmodel, Helper):
@@ -38,34 +38,14 @@ class ProblemSamples(dbmodel, Helper):
   id = dbcol(dblong, primary_key = True)
   pid = dbcol(dbint, dbforkey(Problems.id, onupdate = "CASCADE", ondelete = "CASCADE"), nullable = False)
   ordering = dbcol(dbint, nullable = False)
-  
+
   # i/o, expl
   input = dbcol(dbstr(PROBLEM_SAMPLE_MAX_LENGTH), nullable = False)
   output = dbcol(dbstr(PROBLEM_SAMPLE_MAX_LENGTH), nullable = False)
   explanation = dbcol(dbstr(PROBLEM_SAMPLE_MAX_LENGTH), nullable = False)
-  
+
   # Force pid, ordering to be unique
   __table_args = (dbunicon("pid", "ordering"), )
-  
+
   __tablename__ = "problem_samples"
   
-class ProblemSuites(dbmodel, Helper):
-  id = dbcol(dblong, primary_key = True)
-  pid = dbcol(dbint, dbforkey(Problems.id, onupdate = "CASCADE", ondelete = "CASCADE"), nullable = False)
-  suite = dbcol(dbint, nullable = False)
-  points = dbcol(dbint, nullable = False)
-  
-  # force suite to be unique on a per-problem basis
-  __table_args__ = (dbunicon("pid", "suite"), )
-  
-  __tablename__ = "problem_suites"
-  
-class ProblemCases(dbmodel, Helper):
-  id = dbcol(dblong, primary_key = True)
-  psid = dbcol(dblong, dbforkey(ProblemSuites.id, onupdate = "CASCADE", ondelete = "CASCADE"), nullable = False)
-  case = dbcol(dbint, nullable = False)
-  
-  # force case to be unique on a per-suite basis
-  __table_args__ = (dbunicon("psid", "case"), )
-  
-  __tablename__ = "problem_cases"
